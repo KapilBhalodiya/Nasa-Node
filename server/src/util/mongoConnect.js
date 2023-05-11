71e0b623d28788d5ad16a48c5d961f96d015bb65
= <=>
2023-05-11 17:03:15 +0530

    build first action

--- server/src/util/mongoConnect.js
+++ server/src/util/mongoConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL = "mongodb+srv://admin:dg6mduWYntNbf93v@cluster0.kaxaqkp.mongodb.net/NasaProject" || process.env.MONGO_URL;
 
 mongoose.connection.once('open',() => {
     console.log("Connected!");
